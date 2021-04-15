5ec00b613ee20ce54101eedb401024a068974904
unknown <jackesthalia@gmail.com>
2021-04-15 19:20:58 -0300

    userinfo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState} from 'react'
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
@@ -9,9 +8,9 @@ import Bio from './components/Bio';
 
 function App() {
   const [resultados, mudarResultados] = useState(null)
+  const [userInfo, mudarUserInfo] = useState(null)
 
   var getStared = async (user) => {
-    // const result = await fetch(`https://api.github.com/users/${user}/starred`)
     const results = await fetch(`https://api.github.com/users/${user}/starred`)
       .then((res) => res.json())
       .then((starred) => starred.map((s) => ({
@@ -23,15 +22,22 @@ function App() {
           login:s.owner.login
       })));
       mudarResultados(results)
+
+      const resultsInfo = await fetch(`https://api.github.com/users/${user}`)
+      .then((res) => res.json())
+      
+     
+      mudarUserInfo(resultsInfo)
+
   }
+
 
   return (
     <div className="App">
       <Header/>
       <Search funcaoBuscar={getStared}/>
-      <Bio/>
+      <Bio userInfo={userInfo}/>
       <Results resultados={resultados} />
-      {/* <button onClick={() => getStared()}>clica</button> */}
     </div>
   );
 
--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
 
-function Bio (){
-  return(
+function Bio (props) {
+  const userInfo = props.userInfo;
+  console.log('userInfo', userInfo);
+  if (!userInfo) {
+    return (null)
+  }
+
+
+  return( 
   <div className="bloco-bio">
     <div className="container">
       <div className="card">
         <img className="avatar" src="/img/avatar.svg"/>
-        <h2> Thalia Jackes </h2>
+        <h2>{userInfo.login}</h2>
         <div className="info-bio">
         <h3>Bio</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.­</p>
+        <p>{userInfo.bio}</p>
         </div>
 
         <div className="info-gerais">
@@ -18,21 +25,20 @@ function Bio (){
         
           <div className="email linha">
             <img src="/img/email.svg"/>
-            <p>pedrohemorais@gmail.com</p>
+            <p>{userInfo.email}</p>
           </div>
           <div className="logo-info linha">
             <img src="/img/logo-info.svg"/>
-            <p>Github.com/Pedromorais</p>
+            <p>{userInfo.html_url}</p>
           </div>
           <div className="localizacao linha"> 
             <img src="/img/localizacao.svg"/>
-            <p>Osasco - SP</p>
+            <p>{userInfo.location}</p>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 )}; 
 
   
